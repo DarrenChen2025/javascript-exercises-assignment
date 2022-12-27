53bd9390208ba55244f00971e5a92d9705b0c12e
Darren Chen <chen.darren@ufl.edu>
2022-12-27 14:13:49 -0500

    Completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) 
+{
+  let convert = (temp - 32) * (5/9);
+  let rounded = Math.round(convert * 10) / 10;
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) 
+{
+  let convert = (temp * (9/5)) + 32;
+  let rounded = Math.round(convert * 10) / 10;
+  return rounded
 };
 
 // Do not edit below this line
